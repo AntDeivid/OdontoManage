6644001073a970b16d0358d801a17dd4121f3dbb
AntDeivid <deividsantos121@gmail.com>
2024-08-26 10:57:31 -0300

    tipos e roles (falta a config)

---
+++ OdontoManage/OdontoManage.Core/Enums/Roles.cs
@@ -0,0 +1,7 @@
+namespace OdontoManage.Core.Enums;
+
+public class Roles
+{
+    public const string ADMIN = "ADMINISTRADOR";
+    public const string USER = "USER";
+}
---
+++ OdontoManage/OdontoManage.Core/Enums/UserType.cs
@@ -0,0 +1,7 @@
+namespace OdontoManage.Core.Enums;
+
+public enum UserType
+{
+    ADMINSTRADOR = 1,
+    USER = 2
+}
